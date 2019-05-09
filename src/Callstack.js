cb6c8da4c1f29e88d3a5e0b9f807e5c867ecd604
Andrey Pokrovskiy <apk@apple.com>
2019-05-09 14:13:00 -0700

    Callstack.js looks good

---
+++ src/Callstack.js
@@ -0,0 +1,32 @@
+// Keeps track of current callstack frames and facilitates recursion detection.
+// Initial `level` is 0 (callstack is empty). Frames are usually strings that
+// contain both function and module name (e.g. "fread @ libc")
+export class Callstack {
+  constructor () {
+    this.stack = []
+    this.frameCounts = new Map()
+    this.depth = 0
+  }
+  push (frame) {
+    const frameCounts = this.frameCounts
+    const record = frameCounts.get(frame)
+    if (record) {
+      this.stack[this.depth++] = record
+      return 0 < record.n++
+    }
+    frameCounts.set(frame, (this.stack[this.depth++] = {n: 1}))
+    return false
+  }
+  pop (level) {
+    let depth = this.depth
+    if (level < depth) {
+      const stack = this.stack
+      do { --stack[--depth].n } while (level < depth)
+      this.depth = depth
+    }
+  }
+  recursive (frame) {
+    const record = this.frameCounts.get(frame)
+    return record && 0 < record.n
+  }
+}
