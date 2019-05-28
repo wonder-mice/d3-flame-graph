05b24b7ea405e19c86e9cec11f7b4035242dac06
Andrey Pokrovskiy <apk@apple.com>
2019-05-28 10:55:58 -0700

    index.js: better than keeping it uncommited

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+export {Metrics} from './src/Metrics'
+export {StateInput, State} from './src/State'
+export {Callstack} from './src/Callstack'
+export {StructureTraits, CostTraits, ValueTraits} from './src/Item'
+export {Node, NodeContext} from './src/Node'
 export {
-    deltaColor, nameColor,
-    Metrics, Callstack,
-    ItemTraits,
-    Node, NodeContext, NodeIndexEntry, NodeSelection, NodeHighlightClass, NodeHighlight,
-    markingPredicate, markNodes, markedNodes, markedNodesAggregate,
-    flamegraph
+    DeckOptions, Deck
 } from './src/flamegraph'
