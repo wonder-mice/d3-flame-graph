00c04ec08da76b0bac8fed0ac0fc8eca99395a75
Andrey Pokrovskiy <apk@apple.com>
2019-05-30 15:09:58 -0700

    DeckPage.js: don't use Options creation pattern

--- src/DeckPage.js
+++ src/DeckPage.js
@@ -1,40 +1,18 @@
 import {State} from './State'
-import {StructureTraits, CostTraits, ValueTraits} from './Item'
 import {StructureModel} from './StructureModel'
 import {FlattenModel} from './FlattenModel'
-import {NodeSelection, FlattenNodeSelection, SelectedNodeStructureTraits, selectedNodeStructureRoots} from './NodeSelection'
+import {NodeSelection, FlattenNodeSelection, NodeSelectionStructureTraits} from './NodeSelection'
 import {nodeIndexNodes} from './NodeIndex'
 import {StructureViewOptions, StructureView} from './StructureView'
 import {FlattenViewOptions, FlattenView} from './FlattenView'
 import {SplitView} from './SplitView'
 
-export class DeckPageOptions {
-  constructor () {
-    this.causalDomain = null
-    this.structureRoots = null
-    this.structureTraits = null
-    this.costTraits = null
-    this.valueTraits = null
-    this.nodeTooltipContentCallback = null
-  }
-}
-
 export class DeckPage {
-  // Needed features:
-  // - Option to follow cost/value traits and order function from master model
-  // - Option to setup using selection from master model
-  constructor (parent, options) {
+  constructor (parent, causalDomain) {
     const state = this.state = new State('DeckPage::State')
-    const causalDomain = this.causalDomain = (options && options.causalDomain) || state
-    const nodeTooltipContentCallback = options && options.nodeTooltipContentCallback
-    const structureRoots = (options && options.structureRoots) || null
-    const structureTraits = (options && options.structureTraits) || new StructureTraits()
-    const costTraits = (options && options.costTraits) || new CostTraits()
-    const valueTraits = (options && options.valueTraits) || new ValueTraits()
+    causalDomain = this.causalDomain = causalDomain || state
 
     const element = this.element = document.createElement('div')
-    // FIXME: Remove flamegraph class
-    element.className = 'flamegraph'
     element.style.width = '100%'
     element.style.display = 'flex'
     element.style.position = 'relative'
@@ -50,30 +28,22 @@ export class DeckPage {
 
     const primaryModel = this.primaryModel = new StructureModel()
     primaryModel.rootName = 'FIXME: Primary root name'
-    primaryModel.setStructureRoots(structureRoots)
-    primaryModel.setStructureTraits(structureTraits)
-    primaryModel.setCostTraits(costTraits)
-    primaryModel.setValueTraits(valueTraits)
     const primarySelection = new NodeSelection(primaryModel)
     const primaryViewOptions = new StructureViewOptions()
     primaryViewOptions.causalDomain = causalDomain
     primaryViewOptions.tooltipHostElement = element
     const primaryView = this.primaryView = new StructureView(primaryModel, primaryViewOptions)
-    primaryView.tooltipContentView.setContentCallback(nodeTooltipContentCallback)
     primaryView.tooltipContentView.setSelectionInterface(primarySelection)
     primaryElement.appendChild(primaryView.element)
     state.input(primaryView.state)
 
     const secondaryModel = this.secondaryModel = new FlattenModel()
     secondaryModel.rootName = 'FIXME: Secondary root name'
-    secondaryModel.setStructureTraits(new SelectedNodeStructureTraits())
-    secondaryModel.setCostTraits(costTraits)
-    secondaryModel.setValueTraits(valueTraits)
+    secondaryModel.setStructureTraits(NodeSelectionStructureTraits)
     const secondaryViewOptions = new FlattenViewOptions()
     secondaryViewOptions.causalDomain = causalDomain
     secondaryViewOptions.tooltipHostElement = element
     const secondaryView = this.secondaryView = new FlattenView(secondaryModel, secondaryViewOptions)
-    secondaryView.tooltipContentView.setContentCallback(nodeTooltipContentCallback)
     secondaryView.tooltipContentView.setSelectionInterface(new FlattenNodeSelection(primarySelection))
     secondaryElement.appendChild(secondaryView.element)
     state.input(secondaryView.state)
@@ -99,7 +69,7 @@ export class DeckPage {
     }
 
     this.selectedStructureState = new State('DeckPage::SelectedStructure', (state) => {
-      this.secondaryModel.structureRoots = selectedNodeStructureRoots([this.primaryModel.rootNode])
+      this.secondaryModel.structureRoots = NodeSelectionStructureTraits.selectedRoots([this.primaryModel.rootNode])
     })
     this.selectedStructureState.input(primaryModel.selectionState)
     this.secondaryModel.structureRootsState.input(this.selectedStructureState)
@@ -114,9 +84,6 @@ export class DeckPage {
     if (parent) {
       parent.appendChild(element)
     }
-  }
-  applyStructureRoots (roots) {
-    this.primaryModel.applyStructureRoots(roots)
   }
   setStructureRoots (roots) {
     this.primaryModel.setStructureRoots(roots)
@@ -135,5 +102,9 @@ export class DeckPage {
   setOrderFunction (orderFunction) {
     this.primaryModel.setOrderFunction(orderFunction)
     this.secondaryModel.setOrderFunction(orderFunction)
+  }
+  setNodeTooltipContentCallback (nodeTooltipContentCallback) {
+    this.primaryView.tooltipContentView.setContentCallback(nodeTooltipContentCallback)
+    this.secondaryView.tooltipContentView.setContentCallback(nodeTooltipContentCallback)
   }
 }
