09386ad42a510efbf4b7ed61652cde73aae9cdc2
Andrey Pokrovskiy <apk@apple.com>
2019-05-31 12:56:25 -0700

    Deck.js: tab management

--- src/Deck.js
+++ src/Deck.js
@@ -1,11 +1,24 @@
 import {State} from './State'
 import {TabView} from './TabView'
 import {DeckPage} from './DeckPage'
+import {NodeSelectionStructureTraits} from './NodeSelection'
+
+class DeckItem {
+  constructor (tab, page) {
+    this.tab = tab
+    this.page = page
+  }
+}
 
 export class Deck {
   constructor (parent, causalDomain) {
     const state = this.state = new State('Deck::State')
     causalDomain = this.causalDomain = causalDomain || state
+
+    this.nodeTooltipContentCallback = null
+    this.items = []
+    this.itemNo = 0
+    this.activeItem = null
 
     const element = this.element = document.createElement('div')
     element.style.width = '100%'
@@ -13,12 +26,17 @@ export class Deck {
     element.style.flexDirection = 'column'
 
     const tabView = this.tabView = new TabView(element)
-    tabView.plusTab.element.innerHTML = '<svg fill="currentColor" style="vertical-align:middle" width="12" height="16" viewBox="0 0 12 16"><path fill-rule="evenodd" d="M12 9H7v5H5V9H0V7h5V2h2v5h5v2z"/></svg>'
-    tabView.plusTab.element.addEventListener('click', (event) => {
-      this.addPage('Selection')
-    })
-    const masterPage = this.masterPage = this.addPage('Master', causalDomain)
-    state.input(masterPage.state)
+    const plusTab = this.plusTab = tabView.addTab(null, false)
+    plusTab.element.addEventListener('click', (event) => { this.onPlusTabClick() })
+    plusTab.element.innerHTML = (
+`<svg fill="currentColor" style="vertical-align: middle" width="12" height="16" viewBox="0 0 12 16">
+  <path fill-rule="evenodd" d="M12 9H7v5H5V9H0V7h5V2h2v5h5v2z"/>
+</svg>`)
+
+    const masterPage = this.masterPage = new DeckPage(this.element, this.causalDomain)
+    const masterItem = this.masterItem = this.addPage(masterPage, 'Master')
+    this.setActiveItem(masterItem)
+
     if (parent) {
       parent.appendChild(element)
     }
@@ -30,28 +48,68 @@ export class Deck {
     this.masterPage.setStructureTraits(structureTraits)
   }
   setCostTraits (costTraits) {
-    this.masterPage.setCostTraits(costTraits)
+    for (let items = this.items, i = items.length; i--;) {
+      items[i].page.setCostTraits(costTraits)
+    }
   }
   setValueTraits (valueTraits) {
-    this.masterPage.setValueTraits(valueTraits)
+    for (let items = this.items, i = items.length; i--;) {
+      items[i].page.setValueTraits(valueTraits)
+    }
   }
   setOrderFunction (orderFunction) {
-    this.masterPage.setOrderFunction(orderFunction)
+    for (let items = this.items, i = items.length; i--;) {
+      items[i].page.setOrderFunction(orderFunction)
+    }
   }
   setNodeTooltipContentCallback (nodeTooltipContentCallback) {
-    this.masterPage.setNodeTooltipContentCallback(nodeTooltipContentCallback)
+    this.nodeTooltipContentCallback = nodeTooltipContentCallback
+    for (let items = this.items, i = items.length; i--;) {
+      items[i].page.setNodeTooltipContentCallback(nodeTooltipContentCallback)
+    }
   }
   update () {
     this.causalDomain.update()
   }
-  addPage (name) {
-    const tab = this.tabView.addTab(true)
-    tab.element.innerText = name
-    tab.element.addEventListener('click', (event) => {
-      // activate
-    })
-    const page = new DeckPage(this.element)
+  setActiveItem (item) {
+    const activeItem = this.activeItem
+    if (activeItem !== item) {
+      if (activeItem) {
+        activeItem.page.element.style.display = 'none'
+        activeItem.tab.setActive(false)
+      }
+      item.page.element.style.display = 'flex'
+      item.tab.setActive(true)
+      this.activeItem = item
+    }
+  }
+  addPage (page, name) {
     this.state.input(page.state)
-    return page
+    const tab = this.tabView.addTab(this.plusTab, false)
+    const item = new DeckItem(tab, page)
+    this.items.push(item)
+    tab.element.addEventListener('click', (event) => { this.onItemTabClick(item) })
+    tab.element.innerText = name
+    return item
+  }
+  onPlusTabClick () {
+    const activeItem = this.activeItem
+    const activePage = activeItem.page
+    const activeModel = activePage.primaryModel
+    const page = new DeckPage(this.element, this.causalDomain)
+    page.setStructureRoots(NodeSelectionStructureTraits.selectedRoots([activeModel.rootNode]))
+    page.setStructureTraits(NodeSelectionStructureTraits)
+    page.setStructureCoalescing(true)
+    page.setCostTraits(activeModel.costTraits)
+    page.setValueTraits(activeModel.valueTraits)
+    page.setOrderFunction(activeModel.orderFunction)
+    page.setNodeTooltipContentCallback(this.nodeTooltipContentCallback)
+    const item = this.addPage(page, 'Selection #' + (++this.itemNo))
+    this.setActiveItem(item)
+    this.causalDomain.update()
+  }
+  onItemTabClick (item) {
+    this.setActiveItem(item)
+    this.causalDomain.update()
   }
 }
