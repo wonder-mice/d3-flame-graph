19232e3808d7e040b7c19967bbb5048e5680b364
Andrey Pokrovskiy <apk@apple.com>
2019-06-05 11:18:44 -0700

    index.js: export less

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ export {StateInput, State} from './src/State'
 export {Callstack} from './src/Callstack'
 export {StructureTraits, CostTraits, ValueTraits} from './src/Item'
 export {Node, NodeContext} from './src/Node'
-export {
-    DeckOptions, Deck
-} from './src/flamegraph'
+export {Deck} from './src/Deck'
