363dd0d52191207bf96c4c7acb96b8522fdf61b8
Andrey Pokrovskiy <apk@apple.com>
2019-08-15 15:23:57 -0700

    playground-data.js: add more functions

--- playground-data.js
+++ playground-data.js
@@ -8,7 +8,57 @@ const data = [{
                     n: 'ApplicationInit', v: 100, d: 0
                 }, {
                     n: 'ApplicationRun', v: 200, d: 0, c: [{
-                        n: 'event_loop', v: 195, d: 0
+                        n: 'event_loop', v: 195, d: 0, c: [{
+                            n: 'Function1', v: 10, d: 0
+                        }, {
+                            n: 'Function2', v: 10, d: 0
+                        }, {
+                            n: 'Function3', v: 10, d: 0
+                        }, {
+                            n: 'Function4', v: 10, d: 0
+                        }, {
+                            n: 'Function5', v: 10, d: 0
+                        }, {
+                            n: 'Function6', v: 10, d: 0
+                        }, {
+                            n: 'Function7', v: 10, d: 0
+                        }, {
+                            n: 'Function8', v: 10, d: 0
+                        }, {
+                            n: 'Function9', v: 10, d: 0
+                        }, {
+                            n: 'Function10', v: 10, d: 0
+                        }, {
+                            n: 'Function11', v: 10, d: 0
+                        }, {
+                            n: 'Function12', v: 10, d: 0
+                        }, {
+                            n: 'Function13', v: 10, d: 0
+                        }, {
+                            n: 'Function14', v: 10, d: 0
+                        }, {
+                            n: 'Function15', v: 10, d: 0
+                        }, {
+                            n: 'Function16', v: 10, d: 0
+                        }, {
+                            n: 'Function17', v: 10, d: 0
+                        }, {
+                            n: 'Function18', v: 10, d: 0
+                        }, {
+                            n: 'Function19', v: 10, d: 0
+                        }, {
+                            n: 'Function20', v: 10, d: 0
+                        }, {
+                            n: 'Function21', v: 10, d: 0
+                        }, {
+                            n: 'Function22', v: 10, d: 0
+                        }, {
+                            n: 'Function23', v: 10, d: 0
+                        }, {
+                            n: 'Function24', v: 10, d: 0
+                        }, {
+                            n: 'Function25', v: 10, d: 0
+                        }]
                     }]
                 }]
             }]
