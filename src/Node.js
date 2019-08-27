3817111e70a5e513c8cdf92d41289940fb836cf5
Andrey Pokrovskiy <apk@apple.com>
2019-08-27 10:40:17 -0700

    Nodes.js: consolidated marking, selection, bits, etc. under single flags field

--- src/Node.js
+++ src/Node.js
@@ -1,31 +1,64 @@
 import {StructureTraits} from './Item'
 
-// `Node.mark` flags:
-export const nodeMarked = 0b0001 // node is marked
-export const nodeDescendantMarked = 0b0010 // node has a descendant that is marked
-export const nodeAncestorMarked = 0b0100 // node has an ancestor that is marked
-export const nodeHiddenDescendantMarked = 0b1000 // node has marked descendants that are not visible (e.g. too small)
+// Used with `Node.flags` to support node marking. Marking is different from selection in that
+// it is optimized for full node tree traversal - something like a predicate that says whether
+// node is marked or not. Also marking structure allows to tell whether any specifc node has
+// descendants or ancestors that are marked, but it doesn't allow to tell whether any specific
+// node has descendants or ancestors that are NOT marked.
+export const nodeFlagMarked = 0b001 // node is marked
+export const nodeFlagDescendantMarked = 0b010 // node has a descendant that is marked
+export const nodeFlagAncestorMarked = 0b100 // node has an ancestor that is marked
+export const nodeMaskMark = 0b111
+export const nodeMaskMarkShift = 0
 
-// `Node.bits` flags:
-export const nodeFocused = 0b11 // node is focused
-export const nodeDescendantFocused = 0b10 // node is on the path from focused node to the root
+// Used with `Node.flags` to support node selection. Selection is different from marking in that
+// it is optimized for selecting and unselecting specific set of nodes (i.e. list of nodes is known
+// and full node tree traversal is not needed). Its structure is more symmetrical, in that it's
+// equally easy to get information about what is selected as what is unselected. However this comes
+// at a cost - certain questions (e.g. does this node has (un)selected descendant?) can not be
+// answered easily.
+// When `nodeFlagSelectionTerminator` is set, all node's descendants have the same state of
+// `nodeFlagSelected` flag.  E.g. if node has both `nodeFlagSelected` and `nodeFlagSelectionTerminator`
+// flags set, then the entire subtree is selected. And if node only has `nodeFlagSelectionTerminator`
+// flag set, then the entire subtree is not selected. For leaf nodes `nodeFlagSelectionTerminator`
+// should always be set.
+export const nodeFlagSelected = 0b1000
+export const nodeFlagSelectionTerminator = 0b10000
+export const nodeMaskSelection = 0b11000
+export const nodeMaskSelectionShift = 3
 
-// Flag is set when node is selected.
-export const nodeFlagSelected = 0b000100
-// When set, all node's descendants have the same state of `nodeFlagSelected` flag.  E.g. if
-// node has both `nodeFlagSelected` and `nodeFlagSelectionTerminator` flags set, then the entire
-// subtree is selected. And if node only has `nodeFlagSelectionTerminator` flag set, then the
-// entire subtree is not selected. For leaf nodes `nodeFlagSelectionTerminator` should always
-// be set.
-export const nodeFlagSelectionTerminator = 0b001000
+// Used with `Node.flags` to indicate nodes that provide context for other nodes.
+export const nodeFlagFocused = 0b1100000 // node is focused
+export const nodeFlagDescendantFocused = 0b1000000 // node is on the path from focused node to the root
+export const nodeMaskFocus = 0b1100000
+export const nodeMaskFocusShift = 5
+
+// Used with `Node.flags` to indicate nodes that have marked descendants that are not visible (e.g. too small).
+export const nodeFlagHiddenDescendantMarked = 0b10000000
+
+// Used with `Node.flags` to indicate small nodes. Small nodes can be rendered differently not to
+// include as much of a content, because it'll be not visible anyway.
+export const nodeFlagTiny = 0b100000000
+
+// Used with `Node.flags` to indicate highlighted nodes. Highlight is an amalgam of marking and selection.
+// As selection, it is optimized for cases where set of nodes that needs to be highlighted is known. And
+// as marking, it allows to tell whether node has hidden highlighted descendant. These flags are transient
+// and are cleared by layout. These flags are only valid for layed out nodes - nodes that are not part of
+// current page will have these flags in undeterminite state. Importan difference from marking and
+// selection - for marking and selection, node tree (and node flags) is the source of truth, while for
+// highlight node flags are just a projection of external source of truth (e.g. list of highlighted nodes).
+export const nodeFlagHighlighted = 0b1000000000
+export const nodeFlagHiddenDescendantHighlighted = 0b10000000000
+export const nodeMaskHighlight = 0b11000000000
+export const nodeMaskHighlightShift = 9
 
 export class Node {
   constructor (parent, name, cost) {
     this.parent = parent
     this.name = name
     this.cost = cost
-    this.mark = 0
-    this.bits = 0
+    this.flags = nodeFlagSelected | nodeFlagSelectionTerminator
+    // this.mark = 0
     // Short for `revision`. Value stored in this field is the same for all visible nodes, while not visible nodes
     // will have values different from the value that visible nodes have. This technique saves cycles on marking
     // nodes discarded by layout as invisible. Since usually there are more invisible nodes then visible, it gives
@@ -47,7 +80,6 @@ export class Node {
     //   parent.total == parent.self + sum([child.total for child in parent.children])
     //   node.total >= node.self
     // Layout is free to interpret such cases as it sees fit (pun intended).
-    this.selected = nodeFlagSelected | nodeFlagSelectionTerminator
   }
 }
 
